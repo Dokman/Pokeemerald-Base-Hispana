7952d416a20fc43b46f44a1a50bc335cef280c4a
Dokman <chaoticanthrim@hotmail.com>
2026-04-08 10:04:31 +0200

    Enable Dexnav in PokeNav

--- include/dexnav.h
+++ include/dexnav.h
@@ -71,6 +71,8 @@ enum EncounterType
 
 void EndDexNavSearch(void);
 void Task_OpenDexNavFromStartMenu(u8 taskId);
+void Task_OpenDexNavFromPokenav(u8 taskId);
+u32 PokeNavMenuDexNavCallback(void);
 bool32 TryStartDexNavSearch(void);
 void TryIncrementSpeciesSearchLevel(void);
 void ResetDexNavSearch(void);
